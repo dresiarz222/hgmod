62faa8110e22b726fe6f02abc090e037697e815a
dresiarz222 <79806016+dresiarz222@users.noreply.github.com>
2024-04-11 20:51:25 +0200

    Update main.lua

--- main.lua
+++ main.lua
@@ -213,7 +213,7 @@ function UILib:CreateUI()
 	ImageLabel.BorderSizePixel = 0
 	ImageLabel.Position = UDim2.new(0.155485526, 0, 0.0275019091, 0)
 	ImageLabel.Size = UDim2.new(0.674392283, 0, 0.190794498, 0)
-	ImageLabel.Image = "rbxassetid://15407461567"
+	ImageLabel.Image = getcustomasset("dougware/bg.png")
 	
 	local ScrollHolder = Instance.new("Frame")
 	ScrollHolder.Parent = Sidebar
